e309e82b30d2fc4d8a8961e6fd3b5fbe127c369c
Chady <chady@ualberta.ca>
2018-10-24 21:09:26 -0600

    Added ride requests search and deletion
    Added the main thread, you can enter "O" to see options, if you enter "SMR" you can get to your requests and list them all, then delete the ride id you choose.

--- MiniProject1.py
+++ MiniProject1.py
@@ -8,10 +8,56 @@ def main():
     c = conn.cursor()
     loginEmail = login(c, conn)
     displayMessages(c,conn,loginEmail)
-    # Main while loop
-    #while True():
+    # Main thread
+    while True:
+        answer = input("What would you like to do? Type 'O' for options: ")
+        if(answer.lower() == "o"):
+            print("Type 'SMR' to see your rides\nType 'Exit' to quit.\n")
+        elif(answer.lower() == "exit"):
+            print("Goodbye.")
+            break
+        elif(answer.lower() == "smr"):
+            searchRideRequests(c, conn, loginEmail)
 
     conn.close()
+
+
+# Shows all of the users ride requests
+def searchRideRequests(c, conn, loginEmail):
+    query = "SELECT requests.rid, requests.pickup, requests.dropoff, requests.amount, requests.rdate" \
+            " FROM requests, members WHERE members.email = ? AND requests.email = members.email "
+    rides = runSQL(c, conn, query, (loginEmail,))
+    index = 0
+    limit = 5
+    if(len(rides) == 0):
+        print("You have no ride requests")
+    else:
+        print("Here are your ride requests:\n")
+        # See up to 5 ride requests at a time
+        for ride in rides:
+            # Checks to see if the user wants to see 5 more requests
+            if(index == limit):
+                response = input("If you wish to see 5 more rides, enter 'y', otherwise, enter anything else: ")
+                if(response.lower() == 'y'):
+                    limit += 5
+                else:
+                    break
+            print("Ride ID: %s    Pickup: %s    Dropoff: %s    Price: %s    Date: %s"%(ride[0], ride[1], ride[2], ride[3], ride[4]))
+            index +=1
+        # Allows the user to delete any ride request given
+        while True:
+            answer = input("If you wish to delete a request, simply enter the ride ID. If you wish to go back, enter anything else: ")
+            if(answer.isdigit()):
+                for ride in rides:
+                    if(int(answer) == int(ride[0])):
+                        query = "DELETE FROM requests WHERE rid = ?;"
+                        runSQL(c,conn,query,(answer,))
+                        print("Ride request #%s Deleted."%answer)
+            else:
+                break
+
+
+
 
 # Find out if user wishes to login or register
 def login(c, conn):
@@ -64,17 +110,21 @@ def runSQL(c, conn, query, input):
     conn.commit()
     return info
 
+# Displays all the messages that are unseen for that user
 def displayMessages(c, conn, email):
     query = 'SELECT inbox.sender, inbox.content, inbox.msgTimestamp FROM inbox, members WHERE members.email = ? AND inbox.email = members.email AND inbox.seen = ?'
     values = (email, "n")
+    # Retrieve unseen messages
     messages = runSQL(c, conn, query, values)
     if(len(messages) == 0):
         print("No new messages!")
     else:
+        # Format and print each message
         for v in messages:
             message = "From %s: %s\t\t%s\n" % (v[0], v[1], v[2])
             print(message)
         values = ("y", email)
+        # Update DB to set messages as seen
         query = "UPDATE inbox SET seen = ? WHERE email = ?;"
         runSQL(c, conn, query, values)
 
